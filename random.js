6a5da2e2856d533a249390d420ff06d9981745e4
Evan Pasok <matzo0@rocketmail.com>
2023-11-13 15:53:15 -0500

    formmatted better

--- random.js
+++ random.js
@@ -1,9 +1,11 @@
-function pickRandomHero(heroes) {
-    const randomIndex = Math.floor(Math.random() * heroes.length);
-    return heroes[randomIndex];
+function RandomHero(heroes) {
+    const random = Math.floor(Math.random() * heroes.length);
+    return heroes[random];
 }
 
-const heroesArray = ['Blade', 'Captain America', 'Captain Marvel', 'Deadpool', 'Doctor Strange', 'Ghost Rider', 'Hulk', 'Ironman', 'Magik', 'Morbius', 'Nico Minoru', 'Scarlet Witch', 'Spider-man', 'Storm', 'Venom', 'Wolverine'];
+const heroes = ['Blade', 'Captain America', 'Captain Marvel', 'Deadpool',
+ 'Doctor Strange', 'Ghost Rider', 'Hulk', 'Ironman', 'Magik', 'Morbius',
+  'Nico Minoru', 'Scarlet Witch', 'Spider-man', 'Storm', 'Venom', 'Wolverine'];
 
-const randomHero = pickRandomHero(heroesArray);
+const randomHero = RandomHero(heroes);
 console.log("Randomly picked hero:", randomHero);
